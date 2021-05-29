90c66cc8dd6241c54f3155f0bd39d9ff86d32fc4
sobbey99 <sobbey99@gmail.com>
2021-05-29 11:10:03 +0300

    Scripts Working

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,20 @@
-$(document).ready(function () {
-    window.addEventListener('DOMContentLoaded', () => {
-        const menu = document.querySelector('.header-navphoneversion'),
-        menuItem = document.querySelectorAll('.header-navitem'),
-        hamburger = document.querySelector('.hamburger');
-    
-        hamburger.addEventListener('click', () => {
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.header-navphoneversion'),
+    menuItem = document.querySelectorAll('.header-navitem'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('header-navphoneversion_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
             menu.classList.toggle('header-navphoneversion_active');
-        });
-    
-        menuItem.forEach(item => {
-            item.addEventListener('click', () => {
-                hamburger.classList.toggle('hamburger_active');
-                menu.classList.toggle('header-navphoneversion_active');
-            })
         })
     })
+});
 
         $('.form').validate ( {
             rules: {
@@ -58,4 +57,3 @@ $(document).ready(function () {
         return false
         });
 
-});
